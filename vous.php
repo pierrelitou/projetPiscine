2d3ee8ad72e1ce656dcae2373067cf39c46839e6
danielclaroc <daniel.claro-carvalho@edu.ece.fr>
2018-05-02 16:03:01 +0200

    Mise à jour du code des pages Web

---
+++ vous.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Projet Piscine</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="vous.css">
+	</head>
+	<body>
+		<header>
+			<?php include("bandeau.php");?>
+		</header>
+		
+		<aside>
+		</aside>
+		
+		<main>
+		<br>
+			<div id="cadreinfo">
+				<img src="logoece.jpg" id="photo"><br>
+					<div id="cadresecondaireinfo">
+					</div>
+			</div>
+			<div id="cadrepage">
+				<div id="cadreactivite">
+					<h3> Vos activités sur le réseau </h3>
+				</div>
+			
+				<div id="cadrecompetence">
+					<h3> Vos compétences </h3>
+				</div>
+			
+				<div id="cadreformation">
+					<h3> Vos formations </h3>
+				</div>
+				
+				<div id="cadreexperience">
+					<h3> Vos expériences </h3>
+				</div>
+				
+				<div id="cadreinteret">
+					<h3> Vos centres d'intérêts </h3>
+				</div>
+			</div>
+		</main>
+		
+		<aside>
+		</aside>
+		
+		<footer>
+			<?php include("footer.php");?>
+		</footer>
+	<body>
+<html>
