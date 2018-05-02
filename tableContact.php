0c45f1cbec39c357bbc032c41bcca52052e6ddd9
pierrelitou <pierre.litou@edu.ece.fr>
2018-05-02 13:12:27 +0200

    ajout du footer et autres parties récurentes

---
+++ tableContact.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<style type="text/css">
+		#cadreContact
+		{
+			height: 300px;
+			width: 350px;
+			border:hidden;
+			background-color: rgb(231,245,243);
+			text-align: center;
+
+		}
+	</style>
+</head>
+<body>
+	<div id="cadreContact">
+		<h3>Con</h3>
+		<!-- ajouter le php pour afficher !-->
+	</div>
+</body>
+</html>
